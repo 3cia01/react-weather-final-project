f4771816294b6a9f6b9b9febada6467bdb07cc88
3cia01 <monyepatricia1@gmail.com>
2023-12-09 09:09:47 -0500

    Added axios and updated the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <Weather defaultCity="Lagos" />
+
         <footer>
           This project was coded by Patricia Monye and is {""}
           <a
